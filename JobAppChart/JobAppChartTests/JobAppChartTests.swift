d5454cdc0c822023c4dc63f16809b26133500cb7
Alex <alexanderwills37@gmail.com>
2025-04-28 13:11:58 -0400

    Initial Project Creation

---
+++ JobAppChart/JobAppChartTests/JobAppChartTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobAppChartTests.swift
+//  JobAppChartTests
+//
+//  Created by Alex on 4/28/25.
+//
+
+import Testing
+@testable import JobAppChart
+
+struct JobAppChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
